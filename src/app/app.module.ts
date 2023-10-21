f227fac89adf5d9ee4c53e182971462d11772ca4
jaime-rey <jaimereycasado@gmail.com>
2023-10-22 01:17:23 +0200

    first commit, ngrx tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { StoreModule } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookCollectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
+    HttpClientModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
